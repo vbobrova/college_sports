2e5d856093cf1cddc707ea24f1508dfc1e97fc6c
Vitaliy Bobrov <bobrov1989@gmail.com>
2017-12-14 23:48:09 +0100

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,11 @@
-var mySwiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    paginationHide: false
-});      
+(function(document, Swiper) {
+  document.addEventListener('DOMContentLoaded', function() {
+    var mySwiper = new Swiper('.swiper-container', {
+      loop: true,
+      pagination: '.swiper-pagination',
+      paginationClickable: true,
+      paginationHide: false
+    });
+  });
+})(document, Swiper);
+      
